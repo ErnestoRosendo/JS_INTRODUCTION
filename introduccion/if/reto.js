0c18f2613aaf4e699bcff069161527da51ca8614
Ernesto Rosendo <erosendo@ucol.mx>
2023-03-21 11:36:49 -0600

    Agregue otro ejercicio de condicionales

--- introduccion/if/reto.js
+++ introduccion/if/reto.js
@@ -19,6 +19,7 @@ function result (user, cpu) {
 
 }
 
+
 function validarSuscripcion (tipoDeSuscripcion){
     if (tipoDeSuscripcion === "Free"){
         console.log("Solo puedes tomar los cursos gratis");
@@ -32,4 +33,22 @@ function validarSuscripcion (tipoDeSuscripcion){
         console.log("Digite una sucripcion valida")
     }
 }
+
+//Ahora mismo proceso pero solo usando if, para ello se usa el return
+function validarSuscripcion (tipoDeSuscripcion) {
+	if (tipoDeSuscripcion === "Free"){
+	console.log("Solo puedes tomar los cursos gratis");
+	return;
+    }if (tipoDeSuscripcion === "Basic") {
+	console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+    return;
+    }if (tipoDeSuscripcion === "Expert"){
+	console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+    return;
+    }if (tipoDeSuscripcion === "ExpertDuo") {
+	console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+    return;
+    }
+console.warn('Este tipo de suscripción no existe')
+};
 
