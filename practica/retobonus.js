e19f9dd5822101dd3750293665af42cb3a7631a6
Ernesto Rosendo <erosendo@ucol.mx>
2023-05-03 12:28:20 -0600

    fuction onclick/addEventListener

--- practica/retobonus.js
+++ practica/retobonus.js
@@ -12,3 +12,29 @@ tiposDeSuscripciones[elemento]
 //Asi llamamos al valor que tiene asignado ese elemento
 tiposDeSuscripciones.free
 tiposDeSuscripciones["free"]
+
+function devolverSuscripcion(suscripcion){
+    if (tiposDeSuscripciones[suscripcion]){
+        return tiposDeSuscripciones[suscripcion]
+    }
+    console.warn("Ese tipo de suscripcion no existe")
+}
+
+
+
+const arrayDeSuscripcion =[
+    "Free" ,"Solo puedes tomar los cursos gratis",
+    "Basic" ,"Puedes tomar casi todos los cursos de Platzi durante un mes",
+    "Expert" ,"Puedes tomar casi todos los cursos de Platzi durante un año",
+    "ExpertPlus","Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año",
+  ]
+
+  function devolversub(suscripcion){
+    let pos = arrayDeSuscripcion.indexOf(suscripcion);
+    if (pos != -1){
+      console.log(arrayDeSuscripcion[pos+1])
+      return
+    }
+    console.warn("Esa suscripcion no existe")
+}
+  
